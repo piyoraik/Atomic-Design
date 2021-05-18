23821c6d7a0b1780cfe254799850338fa4cd828e
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-05-18 23:35:51 +0900

    30. Organismの作成2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
+const user = {
+  name: "らいちゅー",
+  image: "https://source.unsplash.com/FilM6ng7VGQ",
+  email: "test@test.com",
+  phone: "090-0000-0000",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
+
 export default function App() {
   return (
     <div className="App">
@@ -11,7 +22,7 @@ export default function App() {
       <SecondaryButton>ボタン</SecondaryButton>
       <br />
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 }
